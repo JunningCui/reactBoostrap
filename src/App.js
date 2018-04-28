da0166d69ea5138735d830dee369d843f5e794d6
cuijunning <cuijunning@didichuxing.com>
2018-04-28 16:56:23 +0800

    submitNew

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
+import Login from './container/Login';
+import Register from './container/Register';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Route path="/register" component={Register}></Route>
+        <Route path="/login" component={Login}></Route>
+      </div>
+    );
+  }
+}
+
+export default App;
